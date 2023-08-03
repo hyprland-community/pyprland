5197977a26f03a5fe72c98f47078d4171723bc57
iliayar <iliayar3@gmail.com>
2023-08-04 02:32:14 +0300

    feat nix: Add nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "pyprland";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+  flake-utils.lib.eachDefaultSystem
+  (system:
+  let
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in
+  {
+    packages = rec {
+      pyprland = pkgs.poetry2nix.mkPoetryApplication {
+        projectDir = ./.;
+        python = pkgs.python310;
+      };
+      default = pyprland;
+    };
+  }
+  );
+}
