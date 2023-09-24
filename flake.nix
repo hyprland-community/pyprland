2b722a39b098c99180c4e99031cc2aae459c7ec6
iliayar <iliayar3@gmail.com>
2023-09-24 14:48:06 +0300

    feat nix: Update nixpkgs to 23.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
