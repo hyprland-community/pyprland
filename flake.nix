b38066ac1df0cad9496ec2342ccbac3915a79162
iliayar <iliayar3@gmail.com>
2023-11-16 02:57:17 +0300

    fix flake.nix: Use external poetry2nix
    Fixes #25

--- flake.nix
+++ flake.nix
@@ -1,27 +1,25 @@
 {
-  description = "pyprland";
+  description = "An hyperland plugin system";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-  flake-utils.lib.eachDefaultSystem
-  (system:
-  let
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-  in
-  {
-    packages = rec {
-      pyprland = pkgs.poetry2nix.mkPoetryApplication {
-        projectDir = ./.;
-        python = pkgs.python310;
-      };
-      default = pyprland;
-    };
-  }
-  );
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; })
+          mkPoetryApplication;
+      in {
+        packages = rec {
+          pyprland = mkPoetryApplication { projectDir = ./.; };
+          default = pyprland;
+        };
+      });
 }
