af6e82261ecfb408aa1ac9b0def3c6c8c68195a8
fdev31 <fdev31@gmail.com>
2024-02-03 23:19:27 +0100

    rework test & enable 3 monitors relayout

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -9,18 +9,44 @@ async def test_relayout():
     await tst.pyprctrl_mock.q.put(b"relayout\n")
     assert tst.subprocess_call.call_count == 1
     calls = set([tuple(al[0][0]) for al in tst.subprocess_call.call_args_list])
-    calls.remove(tuple(("wlr-randr", "--output", "DP-1", "--pos", "1920,0")))
+    calls.remove(
+        (
+            "wlr-randr",
+            "--output",
+            "DP-1",
+            "--pos",
+            "1920,0",
+            "--output",
+            "HDMI-A-1",
+            "--pos",
+            "0,0",
+        )
+    )
 
 
-@pytest.mark.skip(reason="Only two monitors are currently supported")
 @pytest.mark.usefixtures("third_monitor", "sample1_config", "server_fixture")
 @pytest.mark.asyncio
 async def test_broken_relayout():
     await tst.pyprctrl_mock.q.put(b"relayout\n")
     assert tst.subprocess_call.call_count == 1
     calls = set([tuple(al[0][0]) for al in tst.subprocess_call.call_args_list])
-    calls.remove(tuple(("wlr-randr", "--output", "DP-1", "--pos", "1920,0")))
-    calls.remove(tuple(("wlr-randr", "--output", "eDP-1", "--pos", "5360,0")))
+    calls.remove(
+        (
+            "wlr-randr",
+            "--output",
+            "DP-1",
+            "--pos",
+            "1920,0",
+            "--output",
+            "HDMI-A-1",
+            "--pos",
+            "0,0",
+            "--output",
+            "eDP-1",
+            "--pos",
+            "5360,0",
+        )
+    )
 
 
 @pytest.mark.usefixtures("empty_config", "server_fixture")
