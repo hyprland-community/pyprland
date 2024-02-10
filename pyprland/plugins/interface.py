f8f1df1b828335e65aad01abf18e081d24cfb6f1
fdev31 <fdev31@gmail.com>
2024-02-10 18:11:19 +0100

    add a convenient get_client in plugin interface

--- pyprland/plugins/interface.py
+++ pyprland/plugins/interface.py
@@ -1,5 +1,5 @@
 " Common plugin interface "
-from typing import Any
+from typing import Any, cast
 
 from ..common import get_logger
 from ..ipc import getCtrlObjects
@@ -42,3 +42,18 @@ class Plugin:
             self.config.update(config[self.name])
         except KeyError:
             self.config = {}
+
+    async def get_clients(self, mapped=True, workspace=None, workspace_bl=None):
+        "Return the client list, optionally returns only mapped clients or from a given workspace"
+        return [
+            client
+            for client in cast(list[dict[str, Any]], await self.hyprctlJSON("clients"))
+            if (not mapped or client["mapped"])
+            and (
+                workspace is None or cast(str, client["workspace"]["name"]) == workspace
+            )
+            and (
+                workspace_bl is None
+                or cast(str, client["workspace"]["name"]) != workspace_bl
+            )
+        ]
