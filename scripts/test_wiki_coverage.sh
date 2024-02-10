195f9ff43ac89e287dbd9984f2998bb68bf424f3
fdev31 <fdev31@gmail.com>
2024-02-10 18:09:16 +0100

    add a script to check wiki coverage

---
+++ scripts/test_wiki_coverage.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+(for n in $(ls -1 pyprland/plugins/*.py | cut -d/ -f3 |grep -vE '(pyprland|experimental|__|interface|_v[0-9]+.py$)' | sed 's#[.]py$#.md#'); do ls wiki/$n; done > /dev/null) 2>&1 |grep wiki
+
+if [ $? -eq 0 ]; then
+    echo -e "\033[0;31mWIKI coverage FAILED\033[0m: Some pages are not covered "
+    exit 1
+else
+    echo "wiki coverage ok"
+fi
