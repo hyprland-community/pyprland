cf4009bb9d6360a7fa4de4c743159ffd16e40951
fdev31 <fdev31@gmail.com>
2024-02-12 21:38:28 +0100

    add one test

---
+++ tests/test_toggle_dpms.py
@@ -0,0 +1,19 @@
+import asyncio
+from pytest_asyncio import fixture
+from .conftest import mocks
+from .testtools import wait_called
+import pytest
+
+
+@fixture
+async def dpms_config(monkeypatch):
+    d = {"pyprland": {"plugins": ["toggle_dpms"]}}
+    monkeypatch.setattr("tomllib.load", lambda x: d)
+    yield
+
+
+@pytest.mark.asyncio
+async def test_dpms(dpms_config, server_fixture):
+    await mocks.pypr("toggle_dpms")
+    await wait_called(mocks.hyprctl)
+    assert mocks.hyprctl.call_args[0][0] == "dpms off"
