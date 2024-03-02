ac2ccb70267ea296d6b97708aafcfcc151ec2a48
fdev31 <fdev31@gmail.com>
2024-03-02 15:29:20 +0100

    A a script to replace links in RELEASE NOTES

---
+++ scripts/backquote_as_links.py
@@ -0,0 +1,26 @@
+#!/bin/env python
+import re
+import os
+
+
+def replace_links(match):
+    "Substitution handler for regex, replaces backquote items with links when relevant"
+    text = match.group(1)
+    if os.path.exists(f"wiki/{text}.md"):
+        return f"[{text}](https://github.com/hyprland-community/pyprland/wiki/{text})"
+    return f"`{text}`"
+
+
+def main(filename):
+    "Replace `link` with a markdown link if the .md file exists"
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    replaced_content = re.sub(r"`([^`]+)`", replace_links, content)
+
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(replaced_content)
+
+
+if __name__ == "__main__":
+    main("RELEASE_NOTES.md")
