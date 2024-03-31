492cb03f594ebbebb48446473491506eabc9527d
fdev31 <fdev31@gmail.com>
2024-03-31 20:39:14 +0200

    update get-pypr

--- pyprland/command.py
+++ pyprland/command.py
@@ -333,7 +333,7 @@ async def run_client():
     manager = Pyprland()
 
     if sys.argv[1] == "version":
-        print("2.1.0")  # Automatically updated version
+        print("2.1.0-1-gc1e7900")  # Automatically updated version
         return
 
     if sys.argv[1] in ("--help", "-h", "help"):
