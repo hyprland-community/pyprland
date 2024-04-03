19e6528390a96e977406d1e2aae4d1f2a962929b
fdev31 <fdev31@gmail.com>
2024-04-03 20:44:48 +0200

    allow relative change of magnify factor

--- pyprland/plugins/magnify.py
+++ pyprland/plugins/magnify.py
@@ -5,16 +5,34 @@ from .interface import Plugin
 class Extension(Plugin):  # pylint: disable=missing-class-docstring
     zoomed = False
 
+    cur_factor = 1
+
     async def run_zoom(self, *args):
         """[factor] zooms to "factor" or toggles zoom level if factor is ommited"""
-        if args:
+        if args:  # set or update the factor
+            relative = args[0][0] in "+-"
             value = int(args[0])
-            await self.hyprctl(f"misc:cursor_zoom_factor {value}", "keyword")
-            self.zoomed = value != 1
+
+            # compute the factor
+            if relative:
+                self.cur_factor += value
+            else:
+                self.cur_factor = value
+
+            # sanity check
+            if self.cur_factor <= 1.0:
+                self.cur_factor = 1
+
+            # apply the factor
+            self.zoomed = self.cur_factor != 1
+            await self.hyprctl(f"misc:cursor_zoom_factor {self.cur_factor}", "keyword")
         else:  # toggle
             if self.zoomed:
+                self.cur_factor = 1
                 await self.hyprctl("misc:cursor_zoom_factor 1", "keyword")
             else:
-                fact = int(self.config.get("factor", 2))
-                await self.hyprctl(f"misc:cursor_zoom_factor {fact}", "keyword")
+                self.cur_factor = int(self.config.get("factor", 2))
+                await self.hyprctl(
+                    f"misc:cursor_zoom_factor {self.cur_factor}", "keyword"
+                )
             self.zoomed = not self.zoomed
