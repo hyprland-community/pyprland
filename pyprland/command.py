353e258a52995eca279e16a7b062874d2b04ee5e
fdev31 <fdev31@gmail.com>
2024-04-05 21:52:29 +0200

    refactor system_notifier

--- pyprland/command.py
+++ pyprland/command.py
@@ -341,7 +341,7 @@ async def run_client():
     manager = Pyprland()
 
     if sys.argv[1] == "version":
-        print("2.1.4-10")  # Automatically updated version
+        print("2.1.4-11")  # Automatically updated version
         return
 
     if sys.argv[1] in ("--help", "-h", "help"):
--- pyprland/plugins/system_notifier.py
+++ pyprland/plugins/system_notifier.py
@@ -5,19 +5,27 @@ from typing import cast
 from copy import deepcopy
 from .interface import Plugin
 from ..common import apply_filter
+from ..adapters.colors import convert_color
 
 builtin_parsers = {
     "journal": [
         {
-            "pattern": r".*systemd-networkd\[\d+\]: ([a-z0-9]+): Link (UP|DOWN)$",
-            "filter": r"s/.*\[\d+\]: ([a-z0-9]+): Link (UP|DOWN)/\1 is \2/",
+            "pattern": r"([a-z0-9]+): Link UP$",
+            "filter": r"s/.*\[\d+\]: ([a-z0-9]+): Link.*/\1 is active/",
+            "color": "#00aa00",
         },
         {
-            "pattern": r".*systemd-coredump\[\d+\]: .* Process \d+ \(([^)])\) of .* dumped core.$",
-            "filter": r"s/.*Process \d+ \(([^)])\) of .* dumped core./\1 dumped core/",
+            "pattern": r"([a-z0-9]+): Link DOWN$",
+            "filter": r"s/.*\[\d+\]: ([a-z0-9]+): Link.*/\1 is inactive/",
+            "color": "#ff8800",
         },
         {
-            "pattern": r".*usb \d+-[0-9.]+: Product: (.*)",
+            "pattern": r"Process \d+ \(.*\) of .* dumped core.$",
+            "filter": r"s/.*Process \d+ \((.*)\) of .* dumped core./\1 dumped core/",
+            "color": "#aa0000",
+        },
+        {
+            "pattern": r"usb \d+-[0-9.]+: Product: ",
             "filter": r"s/.*usb \d+-[0-9.]+: Product: (.*)/USB plugged: \1/",
         },
     ]
@@ -44,12 +52,12 @@ class Extension(Plugin):
             self.parsers[name] = asyncio.Queue()
             self.log.debug("Loaded parser %s", name)
 
-        for name, props in self.config.get("source", {}).items():
+        for props in self.config.get("sources", []):
             assert (
                 props["parser"] in self.parsers
             ), f"{props['parser']} was not found in {self.parsers}"
-            self.log.debug("Loaded source %s", name)
-            self.tasks.append(asyncio.create_task(self.start_source(name, props)))
+            self.log.debug("Loaded source %s => %s", props["command"], props["parser"])
+            self.tasks.append(asyncio.create_task(self.start_source(props)))
 
     async def exit(self):
         "empty exit function"
@@ -57,35 +65,52 @@ class Extension(Plugin):
         for task in self.tasks:
             task.cancel()
             await task
+        for source in self.sources.values():
+            if source.pid is not None:
+                source.kill()
         self.tasks[:] = []
 
-    async def start_source(self, name, props):
+    async def start_source(self, props):
         "Start a source loop"
-        q = self.parsers[props["parser"]]
+
+        parsers = (
+            [props["parser"]] if isinstance(props["parser"], str) else props["parser"]
+        )
+        queues = [self.parsers[p] for p in parsers]
         proc = await asyncio.create_subprocess_shell(
             props["command"], stdout=asyncio.subprocess.PIPE
         )
-        self.sources[name] = proc
+
+        self.sources[props["command"]] = proc
         assert proc.stdout
+        await asyncio.sleep(1)
         # Read stdout line by line and push to parser
-        while self.running:
+        while proc.returncode is None and self.running:
             line = (await proc.stdout.readline()).decode().strip()
             if line:
-                await q.put(line)
+                for q in queues:
+                    await q.put(line)
 
     async def start_parser(self, name, props: list):
         "Start a parser loop"
         q = self.parsers[name]
-        rules = {}
+        rules = []
+        default_color = self.config.get("default_color", "#5555AA")
         for prop in props:
-            rules["pattern"] = re.compile(prop["pattern"])
-            rules["filter"] = prop["filter"]
+            rules.append(
+                {
+                    "pattern": re.compile(prop["pattern"]),
+                    "filter": prop.get("filter"),
+                    "color": convert_color(prop.get("color", default_color)),
+                }
+            )
         while self.running:
             content = await q.get()
-            for prop in rules:
-                if rules["pattern"].match(content):
-                    if "filter" in rules:
-                        text = apply_filter(content, cast(str, rules["filter"]))
+            for rule in rules:
+                if rule["pattern"].search(content):
+                    if rule["filter"]:
+                        text = apply_filter(content, cast(str, rule["filter"]))
                     else:
                         text = content
-                    await self.notify(text)
+                    await self.notify(text, color=rule["color"])
+                    await asyncio.sleep(0.01)
