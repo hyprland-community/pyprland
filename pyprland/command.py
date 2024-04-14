aa767d796fe6c8409657a8373fe316452142de3b
fdev31 <fdev31@gmail.com>
2024-04-14 23:05:55 +0200

    Version 2.2.6

--- pyprland/command.py
+++ pyprland/command.py
@@ -395,7 +395,7 @@ async def run_client():
     manager = Pyprland()
 
     if sys.argv[1] == "version":
-        print("2.2.5-15")  # Automatically updated version
+        print("2.2.6")  # Automatically updated version
         return
 
     if sys.argv[1] == "edit":
