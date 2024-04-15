8e88a537b1aa5837de33f30a292517c461c2bc68
fdev31 <fdev31@gmail.com>
2024-04-15 21:21:15 +0200

    revert last expose changes

--- pyprland/plugins/expose.py
+++ pyprland/plugins/expose.py
@@ -6,29 +6,24 @@ from ..common import state, CastBoolMixin
 
 
 class Extension(CastBoolMixin, Plugin):  # pylint: disable=missing-class-docstring
-    exposed_clients_list: list[dict] = []
+    exposed: list[dict] = []
 
     @property
     def exposed_clients(self):
         "Returns the list of clients currently using exposed mode"
         if self.cast_bool(self.config.get("include_special"), False):
-            return self.exposed_clients_list
-        return [c for c in self.exposed_clients_list if c["workspace"]["id"] > 0]
-
-    async def move_clients_to_workspace(self, workspace, clients):
-        """Move clients to a workspace"""
-        return [
-            f"movetoworkspacesilent {workspace},address:{client['address']}"
-            for client in clients
-        ]
+            return self.exposed
+        return [c for c in self.exposed if c["workspace"]["id"] > 0]
 
     async def run_expose(self):
         """Expose every client on the active workspace.
         If expose is active restores everything and move to the focused window"""
-        if self.exposed_clients_list:
-            commands = self.move_clients_to_workspace(
-                state.active_workspace, self.exposed_clients
-            )
+        if self.exposed:
+            commands = []
+            for client in self.exposed_clients:
+                commands.append(
+                    f"movetoworkspacesilent {client['workspace']['id']},address:{client['address']}"
+                )
             commands.extend(
                 [
                     "togglespecialworkspace exposed",
@@ -36,13 +31,13 @@ class Extension(CastBoolMixin, Plugin):  # pylint: disable=missing-class-docstri
                 ]
             )
             await self.hyprctl(commands)
-            self.exposed_clients_list = []
+            self.exposed = []
         else:
-            self.exposed_clients_list = await self.get_clients(
-                workspace_bl=state.active_workspace
-            )
-            commands = self.move_clients_to_workspace(
-                "special:exposed", self.exposed_clients
-            )
+            self.exposed = await self.get_clients(workspace_bl=state.active_workspace)
+            commands = []
+            for client in self.exposed_clients:
+                commands.append(
+                    f"movetoworkspacesilent special:exposed,address:{client['address']}"
+                )
             commands.append("togglespecialworkspace exposed")
             await self.hyprctl(commands)
