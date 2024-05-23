96cdd28943affe86e17d314c0c9d24dd53bb8001
fdev31 <fdev31@gmail.com>
2024-05-23 19:32:33 +0200

    Always default to a recent Hyprland on broken builds

--- pyprland/plugins/pyprland.py
+++ pyprland/plugins/pyprland.py
@@ -6,6 +6,8 @@ from ..common import MINIMUM_ADDR_LEN, state
 from ..types import VersionInfo
 from .interface import Plugin
 
+DEFAULT_VERSION = VersionInfo(9, 9, 9)
+
 
 class Extension(Plugin):
     """Internal built-in plugin allowing caching states and implementing special commands."""
@@ -13,25 +15,30 @@ class Extension(Plugin):
     async def init(self) -> None:
         """Initialize the plugin."""
         state.active_window = ""
+        version_str = ""
         try:
-            version_info = await self.hyprctl_json("version")
+            version_info: None | dict[str, str | bool | list] = await self.hyprctl_json("version")
+            assert version_info
         except json.JSONDecodeError:
             self.log.exception("Fail to parse hyprctl version")
-            await self.notify_error("Error: 'hyprctl version': incorrect JSON data")
-            version = "v0.0.0"
+            await self.notify_error("Error: 'hyprctl version' didn't print valid data")
         else:
-            if version_info.get("tag"):
-                version = version_info["tag"].split("-", 1)[0]
-            else:
-                version = "v9.9.9"
-                self.log.warning("No tag available, assuming a recent git version.")
+            _tag = version_info.get("tag")
+            if _tag:
+                assert isinstance(_tag, str)
+                version_str = _tag.split("-", 1)[0]
 
-        try:
-            state.hyprland_version = VersionInfo(*(int(i) for i in version[1:].split(".")[:3]))
-        except Exception:  # pylint: disable=broad-except
-            self.log.exception('Fail to parse version tag "%s"', version)
-            await self.notify_error(f"Failed to parse hyprctl version tag: {version}")
-            state.hyprland_version = VersionInfo(0, 0, 0)
+        if version_str:
+            try:
+                state.hyprland_version = VersionInfo(*(int(i) for i in version_str[1:].split(".")[:3]))
+            except Exception:  # pylint: disable=broad-except
+                self.log.exception('Fail to parse version tag "%s"', version_str)
+                await self.notify_error(f"Failed to parse hyprctl version tag: {version_str}")
+                version_str = ""
+
+        if not version_str:
+            self.log.error("Fail to parse version information: %s", version_info)
+            state.hyprland_version = DEFAULT_VERSION
 
         state.active_workspace = (await self.hyprctl_json("activeworkspace"))["name"]
         monitors = await self.hyprctl_json("monitors")
@@ -59,9 +66,9 @@ class Extension(Plugin):
             state.active_window = "0x" + addr
             self.log.debug("active_window = %s", state.active_window)
 
-    async def event_workspace(self, wrkspace: str) -> None:
+    async def event_workspace(self, workspace: str) -> None:
         """Track the active workspace."""
-        state.active_workspace = wrkspace
+        state.active_workspace = workspace
         self.log.debug("active_workspace = %s", state.active_workspace)
 
     async def event_focusedmon(self, mon: str) -> None:
