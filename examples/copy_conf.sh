d830bfa20c229e104f699b786ce81999e0823a57
fdev31 <fdev31@gmail.com>
2024-05-25 17:23:42 +0200

    copy the .d folder

--- examples/copy_conf.sh
+++ examples/copy_conf.sh
@@ -11,6 +11,9 @@ for fname in ${FILENAMES[@]}; do
     install -T ~/.config/hypr/$fname $name/hypr/$fname
 done
 
+# recursively install the ~/config/hypr/pyprland.d folder into $name/hypr/pyprland.d
+cp -r ~/.config/hypr/pyprland.d $name/hypr/
+
 for fname in "config" "style.scss" ; do
     install -T ~/.config/gBar/$fname $name/hypr/gBar/$fname
 done
