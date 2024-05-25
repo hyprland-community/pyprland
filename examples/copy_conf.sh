70dd8817303b4f7a37e7dae85aa4f0e6d4622dcc
fdev31 <fdev31@gmail.com>
2024-05-25 17:16:05 +0200

    update copy_conf to include gbar

--- examples/copy_conf.sh
+++ examples/copy_conf.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-echo -n "config name: "
-read name
+if [ -z "$1" ]; then
+    echo -n "config name: "
+    read name
+else
+    name=$1
+fi
 [ -d $name/hypr/ ] || mkdir $name/hypr/
 FILENAMES=("hyprland.conf" "pyprland.toml")
 for fname in ${FILENAMES[@]}; do
     install -T ~/.config/hypr/$fname $name/hypr/$fname
+done
+
+for fname in "config" "style.scss" ; do
+    install -T ~/.config/gBar/$fname $name/hypr/gBar/$fname
 done
