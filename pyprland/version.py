c36c68b86f15bf1cc608ec20b116df7d5ba41277
fdev31 <fdev31@gmail.com>
2024-06-01 01:08:32 +0200

    add more navigation links

--- pyprland/version.py
+++ pyprland/version.py
@@ -1,3 +1,3 @@
 """Package version."""
 
-VERSION = "2.3.5-12"
+VERSION = "2.3.5-13"
--- site/.vitepress/config.mjs
+++ site/.vitepress/config.mjs
@@ -20,6 +20,31 @@ export default defineConfig({
       {
         text: 'Navigation',
         items: menu
+      },
+      {
+        text: 'standard plugins',
+        items: [
+          { text: "Expose", link: "expose" },
+          { text: "Fetch_client_menu", link: "fetch_client_menu" },
+          { text: "Gbar", link: "gbar" },
+          { text: "Layout center", link: "layout_center" },
+          { text: "Lost_windows", link: "lost_windows" },
+          { text: "Magnify", link: "magnify" },
+          { text: "Monitors", link: "monitors" },
+          { text: "Scratchpads", link: "scratchpads" },
+          { text: "Shortcuts menu", link: "shortcuts_menu" },
+          { text: "System notifier", link: "system_notifier" },
+          { text: "Toggle dpms", link: "toggle_dpms" },
+          { text: "Toggle special", link: "toggle_special" },
+          { text: "Wallpapers", link: "wallpapers" },
+        ]
+      }, {
+        text: 'multi-monitor plugins',
+        items: [
+          { text: "Monitors", link: "monitors" },
+          { text: "Shift monitors", link: "shift_monitors" },
+          { text: "Workspaces_follow focus", link: "workspaces_follow_focus" },
+        ]
       }
     ],
 
