ea820c7d2283c2fe0c93d42b35fe4c64f49383da
fdev31 <fdev31@gmail.com>
2024-06-01 00:37:26 +0200

    leave config in index

--- pyprland/version.py
+++ pyprland/version.py
@@ -1,3 +1,3 @@
 """Package version."""
 
-VERSION = "2.3.5-10"
+VERSION = "2.3.5-11"
--- site/.vitepress/config.mjs
+++ site/.vitepress/config.mjs
@@ -7,23 +7,9 @@ export default defineConfig({
   description: "The official Pyprland website",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/fdev31/pyprland' }
     ]
   }
 })
