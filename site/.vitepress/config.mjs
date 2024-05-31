60ebd85e3f149e3f86c9fd53cba1a5227e0f2a21
fdev31 <fdev31@gmail.com>
2024-06-01 00:45:45 +0200

    add a sidebar menu

--- site/.vitepress/config.mjs
+++ site/.vitepress/config.mjs
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vitepress'
 
+const menu = [
+  { text: 'Getting started', link: '/Getting-started' },
+  { text: 'Plugins', link: '/Plugins' },
+  { text: 'Troubleshooting', link: '/Troubleshooting' },
+  { text: 'Development', link: '/Development' },
+]
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Pyprland web",
@@ -7,6 +14,14 @@ export default defineConfig({
   description: "The official Pyprland website",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    nav: menu,
+
+    sidebar: [
+      {
+        text: 'Navigation',
+        items: menu
+      }
+    ],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/fdev31/pyprland' }
