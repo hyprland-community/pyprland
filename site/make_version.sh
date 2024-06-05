48f329c795ae16ae13e096440258d7dc0acaedaf
fdev31 <fdev31@gmail.com>
2024-06-05 20:57:02 +0200

    update the make version script

--- site/make_version.sh
+++ site/make_version.sh
@@ -6,4 +6,7 @@ else
 fi
 mkdir versions/$version
 cp *.md versions/$version/
+sed -i '/## What/,$d' versions/$version/index.md
+echo "## Version $version archive" >> versions/$version/index.md
 git add versions/$version/*.md
+git commit . -m "Archive documentation for version $version"
