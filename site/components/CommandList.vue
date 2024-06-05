2f27a63206285f2fa93776dd2b319c2d22fbce9d
fdev31 <fdev31@gmail.com>
2024-06-05 22:40:07 +0200

    trim whitespaces just in case

--- site/components/CommandList.vue
+++ site/components/CommandList.vue
@@ -2,7 +2,8 @@
     <dl>
     </dl>
     <ul v-for="command in commands" :key="command.name">
-        <li><code v-html="command.name.replace(/ /g, '&ensp; ')" /> <span v-html="command.description" /></li>
+        <li><code v-html="command.name.replace(/[   ]*$/, '').replace(/ /g, '&ensp; ')" /> <span
+                v-html="command.description" /></li>
     </ul>
 </template>
 
