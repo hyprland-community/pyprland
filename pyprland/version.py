28d0ab2d216dac50c4db8572acb2cdfdd8ccdb4b
fdev31 <fdev31@gmail.com>
2026-01-03 22:52:34 +0100

    site: fix plugins order (alphabetical)

--- pyprland/version.py
+++ pyprland/version.py
@@ -1,3 +1,3 @@
 """Package version."""
 
-VERSION = "2.5.0-82"
+VERSION = "2.5.0-83"
--- site/.vitepress/config.mjs
+++ site/.vitepress/config.mjs
@@ -1,39 +1,47 @@
 const enableVersions = true;
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
-const version_names = ['2.3.5', '2.3.6,7', '2.3.8', '2.4.0', '2.4.1+', '2.4.6', '2.4.7', '2.5.x']
+const version_names = [
+  "2.3.5",
+  "2.3.6,7",
+  "2.3.8",
+  "2.4.0",
+  "2.4.1+",
+  "2.4.6",
+  "2.4.7",
+  "2.5.x",
+];
 
 const extra_versions = {
   items: [
     {
-      text: 'Current',
-      link: '/'
-    }
-  ]
-}
-
+      text: "Current",
+      link: "/",
+    },
+  ],
+};
 
 for (const version of version_names) {
   extra_versions.items.push({
     text: version,
-    link: `/versions/${version}/`
-  })
+    link: `/versions/${version}/`,
+  });
 }
-
 
 const menu = [
   {
-    text: 'Versions',
+    text: "Versions",
     items: [extra_versions],
-  }
-]
+  },
+];
 
 const plugin_list = {
-  text: 'Featured plugins',
+  text: "Featured plugins",
   collapsible: true,
   collapsed: false,
   items: [
     { text: "Expose", link: "./expose" },
+    { text: "Fcitx5 Switcher", link: "./fcitx5_switcher" },
     { text: "Fetch client menu", link: "./fetch_client_menu" },
     { text: "Menubar", link: "./menubar" },
     { text: "Layout center", link: "./layout_center" },
@@ -41,12 +49,12 @@ const plugin_list = {
     { text: "Magnify", link: "./magnify" },
     { text: "Monitors", link: "./monitors" },
     {
-      text: "Scratchpads", link: "./scratchpads",
+      text: "Scratchpads",
+      link: "./scratchpads",
       items: [
         { text: "Advanced", link: "./scratchpads_advanced" },
-        { text: "Special cases", link: "./scratchpads_nonstandard" }
-
-      ]
+        { text: "Special cases", link: "./scratchpads_nonstandard" },
+      ],
     },
     { text: "Shift monitors", link: "./shift_monitors" },
     { text: "Shortcuts menu", link: "./shortcuts_menu" },
@@ -55,9 +63,8 @@ const plugin_list = {
     { text: "Toggle special", link: "./toggle_special" },
     { text: "Wallpapers", link: "./wallpapers" },
     { text: "Workspaces follow focus", link: "./workspaces_follow_focus" },
-    { text: "Fcitx5 Switcher", link: "./fcitx5_switcher" }
-  ]
-}
+  ],
+};
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -67,9 +74,9 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: menu,
-    logo: '/icon.png',
+    logo: "/icon.png",
     search: {
-      provider: 'local',
+      provider: "local",
       options: {
         miniSearch: {
           // options: {
@@ -77,16 +84,16 @@ export default defineConfig({
           // },
           searchOptions: {
             processTerm: (text) => {
-              console.log(text)
+              console.log(text);
               // if matching '/versions/' return null
-              if (text.includes('/versions/')) {
-                return null
+              if (text.includes("/versions/")) {
+                return null;
               }
               return text;
-            }
-          }
-        }
-      }
+            },
+          },
+        },
+      },
     },
     outline: {
       level: [2, 3],
@@ -94,28 +101,31 @@ export default defineConfig({
     sidebar: {
       collapsible: true,
       collapsed: true,
-      '/versions': [
-        { text: 'Getting started', link: './Getting-started' },
-        { text: 'Plugins', link: './Plugins' },
-        { text: 'Troubleshooting', link: './Troubleshooting' },
-        { text: 'Development', link: './Development' },
+      "/versions": [
+        { text: "Getting started", link: "./Getting-started" },
+        { text: "Plugins", link: "./Plugins" },
+        { text: "Troubleshooting", link: "./Troubleshooting" },
+        { text: "Development", link: "./Development" },
         plugin_list,
         {
-          text: 'Return to latest version',
-          link: '/'
-        }
+          text: "Return to latest version",
+          link: "/",
+        },
       ],
-      '/': [
-        { text: 'Getting started', link: './Getting-started' },
-        { text: 'Plugins', link: './Plugins' },
-        { text: 'Troubleshooting', link: './Troubleshooting' },
-        { text: 'Development', link: './Development' },
+      "/": [
+        { text: "Getting started", link: "./Getting-started" },
+        { text: "Plugins", link: "./Plugins" },
+        { text: "Troubleshooting", link: "./Troubleshooting" },
+        { text: "Development", link: "./Development" },
         plugin_list,
-      ]
+      ],
     },
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/fdev31/pyprland' },
-      { icon: 'discord', link: 'https://discord.com/channels/1055990214411169892/1230972154330218526' }
-    ]
-  }
-})
+      { icon: "github", link: "https://github.com/fdev31/pyprland" },
+      {
+        icon: "discord",
+        link: "https://discord.com/channels/1055990214411169892/1230972154330218526",
+      },
+    ],
+  },
+});
