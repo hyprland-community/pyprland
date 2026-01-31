f5f9396e0756ae185bc56057ff825bb2453510c3
fdev31 <fdev31@gmail.com>
2026-01-31 19:18:22 +0100

    prettier command list

--- site/components/PluginCommands.vue
+++ site/components/PluginCommands.vue
@@ -1,15 +1,17 @@
 <template>
-  <div v-if="loading">Loading commands...</div>
-  <div v-else-if="error">{{ error }}</div>
-  <ul v-else>
-    <li v-for="command in commands" :key="command.name">
-      <code>{{ command.name }}</code>
-      <template v-for="(arg, idx) in command.args" :key="idx">
-        &nbsp;<code>{{ formatArg(arg) }}</code>
-      </template>
-      &nbsp;<span v-html="renderDescription(command.short_description)" />
-    </li>
-  </ul>
+  <div v-if="loading" class="command-loading">Loading commands...</div>
+  <div v-else-if="error" class="command-error">{{ error }}</div>
+  <div v-else class="command-box">
+    <ul class="command-list">
+      <li v-for="command in commands" :key="command.name" class="command-item">
+        <code class="command-name">{{ command.name }}</code>
+        <template v-for="(arg, idx) in command.args" :key="idx">
+          <code class="command-arg">{{ formatArg(arg) }}</code>
+        </template>
+        <span class="command-desc" v-html="renderDescription(command.short_description)" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
