34c46796aca52c3a43a79d9725577b1826a26244
fdev31 <fdev31@gmail.com>
2026-09-16 20:06:23 +0200

    update copy_conf script (assume lua)

--- examples/copy_conf.sh
+++ examples/copy_conf.sh
@@ -6,9 +6,21 @@ else
     name=$1
 fi
 [ -d $name/hypr/ ] || mkdir $name/hypr/
-FILENAMES=("hyprland.conf" "pyprland.toml")
+FILENAMES=("hyprland.lua")
 for fname in ${FILENAMES[@]}; do
     install -T ~/.config/hypr/$fname $name/hypr/$fname
+done
+
+[ -d $name/hypr/ ] || mkdir $name/hypr/
+FILENAMES=("hyprland.lua")
+for fname in ${FILENAMES[@]}; do
+    install -T ~/.config/hypr/$fname $name/hypr/$fname
+done
+
+[ -d $name/pypr/ ] || mkdir $name/pypr/
+FILENAMES=("config.toml" "conf.d")
+for fname in ${FILENAMES[@]}; do
+    install -T ~/.config/pypr/$fname $name/pypr/$fname
 done
 
 # recursively install the ~/config/hypr/pyprland.d folder into $name/hypr/pyprland.d
